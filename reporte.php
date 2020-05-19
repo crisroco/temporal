88f22c48339c05acaecda0bc270ea008d206cc13
crojas <christianrojascollazos@gmail.com>
2020-05-18 22:13:07 -0500

    test descarga

--- reporte.php
+++ reporte.php
@@ -7,30 +7,46 @@ global $DB, $USER;
 
  
 
-require_login();
+// require_login();
 
-if(!isset($_GET['group']) || !isset($_GET['scorm']) ){
-	echo 'Falta el parametro';
-	die();
+// if(!isset($_GET['group']) || !isset($_GET['scorm']) ){
+// 	echo 'Falta el parametro';
+// 	die();
+// }
+
+// $cursos = get_course_by_categoria_id($_GET['group']);
+// echo "<pre>";
+// print_r($cursos);
+// echo "</pre>";
+
+// die();
+
+
+
+$lista = array (
+    array('aaa', 'bbb', 'ccc', 'dddd'),
+    array('123', '456', '789'),
+    array('"aaa"', '"bbb"')
+);
+
+$fp = fopen('fichero.csv', 'w');
+
+foreach ($lista as $campos) {
+    fputcsv($fp, $campos);
 }
 
-$cursos = get_course_by_categoria_id($_GET['group']);
-echo "<pre>";
-print_r($cursos);
-echo "</pre>";
+fclose($fp);
 
-die();
 
-// $filename = 'Reporte_'. $nombre ."_".$hoy.'.xlsx';
-// $writer->save($filename);
-// //header("Content-type:xlsx");
-// header('Content-Description: File Transfer');
-// header('Content-Type: application/vnd.ms-excel');
-// header("Content-disposition: attachment; filename=$filename");
-// //header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-// header('Expires: 0');
-// header('Cache-Control: must-revalidate');
-// header('Pragma: public');
-// header('Content-Length: ' . filesize($filename));
-// readfile("$filename");
-// unlink($filename);
+$filename = 'fichero.csv';
+//header("Content-type:xlsx");
+header('Content-Description: File Transfer');
+header('Content-Type: application/vnd.ms-excel');
+header("Content-disposition: attachment; filename=$filename");
+//header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filename));
+readfile("$filename");
+unlink($filename);
